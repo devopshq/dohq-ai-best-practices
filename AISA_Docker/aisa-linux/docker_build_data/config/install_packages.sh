ab323410e977209890aa002a0cedba8cf7f705a7
Ragulin Dmitry <dvragulin@gmail.com>
2021-03-29 15:32:47 +0300

    Update docs for Dockerfile

--- AISA_Docker/aisa-linux/docker_build_data/config/install_packages.sh
+++ AISA_Docker/aisa-linux/docker_build_data/config/install_packages.sh
@@ -3,14 +3,14 @@
 
 # --- Variables ------------------------
 
-ARTIFACTORY_LOGIN=$1
-ARTIFACTORY_PASSWORD=$2
+REGISTRY_LOGIN=$1
+REGISTRY_PASSWORD=$2
 AISA_PACKAGE=$3
-ARTIFACTORY_URL="https://repo.<your_repo>>.com:443"
-ARTIFACTORY_REPO="your_repo"
+REGISTRY_URL="https://repo.<your_repo>>.com:443"
+REGISTRY_REPO="your_repo"
 
 # --- Download & install Aisa ----------
 set -ex
 
-wget --user ${ARTIFACTORY_LOGIN} --password ${ARTIFACTORY_PASSWORD} "${ARTIFACTORY_URL}/${ARTIFACTORY_REPO}/${AISA_PACKAGE}"
+wget --user ${REGISTRY_LOGIN} --password ${REGISTRY_PASSWORD} "${REGISTRY_URL}/${REGISTRY_REPO}/${AISA_PACKAGE}"
 dpkg -i ${AISA_PACKAGE}
